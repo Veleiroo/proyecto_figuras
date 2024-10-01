7169d8045d4e4c6604707692b89bbe0209a647af
Veleiroo <pablo.perez.rodriguez2@rai.usc.es>
2024-10-01 18:59:30 +0200

    Agregadas funcionalidades para el rectángulo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from lib import cuadrado
+from lib import cuadrado, rectangulo
 print("Proyecto Figuras")
 
 # Para el cuadrado
@@ -7,3 +7,10 @@ print(cuadrado.get_identificador())
 print(f"El área de un cuadrado de lado {lado} es: {cuadrado.get_area(lado)}"
       "y el perímetro es: {cuadrado.get_perimetro(lado)}")
 
+# Para el rectángulo
+base = 4
+altura = 2
+print(rectangulo.get_identificador())
+print(f"El área de un {rectangulo.get_identificador()} de base {base} "
+      f"y altura {altura} es: {rectangulo.get_area(base, altura)}"
+      f"y el perímetro es: {rectangulo.get_perimetro(base, altura)}")
